3729004a09636654f1b186300d7c1760aabf12f2
salino3 <flaviobcn012@gmail.com>
2023-06-01 17:27:13 +0200

    create hooks folder

--- src/pods/update/update.component.tsx
+++ src/pods/update/update.component.tsx
@@ -1,17 +1,103 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { GlobalContext, MyState, Posts } from '@/core';
 import * as classes from './update.styles';
 
 export const UpdateComponent: React.FC = () => {
 
   const params = useParams();
+  const {state} = React.useContext<MyState>(GlobalContext);
+  const post: Posts = state.posts.filter((post: Posts) => post.id === Number(params.id))[0];
+
+ 
+   const [dataForm, setDataForm] = React.useState<Posts>(post);
+
+const handleChange = (index: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
+  const keyWords = [...dataForm.keyWords];
+  keyWords[index] = event?.target?.value;
+  setDataForm({...dataForm,   keyWords: keyWords});
+
+};
+
+
+function handleSubmit () {
+
+};
+console.log(post && post?.title);
 
   return (
-    <div className={classes.root}>
-      <h1>Update Post nº {params?.id}</h1>
-     <form>
-
-     </form>
-    </div>
+    <main className={classes.root}>
+      <h1>Update Post nº {dataForm && dataForm?.id}</h1>
+      <div>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <div className={""}>
+            <label htmlFor="title">Title</label>
+            <input
+              placeholder="Text a title"
+              id="title"
+              name="title"
+              type="text"
+              value={dataForm?.title}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="description">Description</label>
+            <textarea
+              id="description"
+              placeholder="Text a description"
+              name="description"
+              value={dataForm?.description}
+              required
+            ></textarea>
+          </div>
+          <div className={""}>
+            <label htmlFor="img">
+              Image <small>(not required)</small>
+            </label>
+            <textarea
+              name="img"
+              placeholder="Paste your URL image"
+              id="img"
+              cols={30}
+              rows={4}
+              value={dataForm?.img}
+            ></textarea>
+          </div>
+          <div className={""}>
+            <label htmlFor="keyWords">
+              Key Words <br /> <small>(only first one required)</small>
+            </label>
+            <input
+              placeholder="Text first key word"
+              name="keyWords[0]"
+              type="text"
+              required
+              value={dataForm?.keyWords[0]}
+            />
+          </div>
+          <div className={""}>
+            <input
+              placeholder="Text second key word"
+              name="keyWords[1]"
+              type="text"
+              value={dataForm?.keyWords[1]}
+              onChange={handleChange(1)}
+            />
+          </div>
+          <div className={""}>
+            <input
+              placeholder="Text third key word"
+              name="keyWords[2]"
+              type="text"
+              value={dataForm?.keyWords[2]}
+            />
+          </div>
+          <div className={""}>
+            <button type="submit">Send</button>
+          </div>
+        </form>
+      </div>
+    </main>
   );
 }
--- src/pods/update/update.styles.ts
+++ src/pods/update/update.styles.ts
@@ -4,6 +4,24 @@ export const root = css`
   height: 100%;
   margin-top: 90px;
   padding: 15px;
+
+`;
+
+
+export const form = css`
+  display: flex;
+  flex-direction: column;
+  gap: 18px;
+  border: solid #03fcfcdd 3px;
+  padding: 38px;
+  background-color: #25007b6e;
+  border-radius: 12px 92px;
+  margin-bottom: 52px;
+
+  label {
+    align-self: center;
+    font-weight: 600;
+  }
 `;
 
 
